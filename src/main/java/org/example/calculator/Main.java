d373e99c656e301072e1ea1b1c7071de0a1a889b
HyeonJun <osumainaddict527@gmail.com>
2025-05-02 01:39:10 +0900

    레벨2 구현

--- src/main/java/org/example/calculator/Main.java
+++ src/main/java/org/example/calculator/Main.java
@@ -4,6 +4,8 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+
             try {
                 Scanner scanner = new Scanner(System.in);
                 do {
@@ -24,17 +26,10 @@ public class Main {
                     가 뜨는 것이다. 그 이유는 개행문자가 그대로 남아. nextLine()이 버퍼에 있었던 개행문자를 읽었다.
                     그리고 즉시 if문이 false가 되어 다시 시작되었다. 즉 무한 반복하는 프로그램을 만든것.
                      */
-                    int result = switch (s) {
-                        case '+' -> a + b;
-                        case '-' -> a - b;
-                        case '*' -> a * b;
-                        case '/' -> {
-                            if (b == 0) throw new ArithmeticException("나눗셈 연산에서 분모(두 번째 정수)에 0이 입력될 수 없다.");
-                            yield a / b;
-                        }
-                        default -> throw new RuntimeException("지원하지 않는 연산자 : " + s);
-                    };
-                    System.out.println("결과 : " + result);
+
+                    int calced = calculator.calc(a, b, s);
+                    calculator.removeResult();
+                    System.out.println("결과 : " + calced);
                     System.out.print("종료하시려면 exit, 아니면 아무 키나 입력해주세요 : ");
                 } while (!scanner.nextLine().equals("exit"));
             } catch (RuntimeException e) {
