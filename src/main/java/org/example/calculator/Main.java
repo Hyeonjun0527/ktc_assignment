276574e050d6a82df03286b426d149368753e54d
HyeonJun <osumainaddict527@gmail.com>
2025-05-02 02:24:13 +0900

    레벨3

--- src/main/java/org/example/calculator/Main.java
+++ src/main/java/org/example/calculator/Main.java
@@ -4,15 +4,15 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Calculator calculator = new Calculator();
+        ArithmeticCalculator<Double> arithmeticCalculator = new ArithmeticCalculator<>();
 
             try {
                 Scanner scanner = new Scanner(System.in);
                 do {
                     System.out.print("첫 번째 숫자를 입력하세요: ");
-                    int a = scanner.nextInt();
+                    double a = scanner.nextDouble();
                     System.out.print("두 번째 숫자를 입력하세요: ");
-                    int b = scanner.nextInt();
+                    double b = scanner.nextDouble();
                     System.out.print("사칙연산 기호를 입력하세요: ");
                     char s = scanner.next().charAt(0);
                     scanner.nextLine();
@@ -26,10 +26,16 @@ public class Main {
                     가 뜨는 것이다. 그 이유는 개행문자가 그대로 남아. nextLine()이 버퍼에 있었던 개행문자를 읽었다.
                     그리고 즉시 if문이 false가 되어 다시 시작되었다. 즉 무한 반복하는 프로그램을 만든것.
                      */
-
-                    int calced = calculator.calc(a, b, s);
-                    calculator.removeResult();
+                    double calced = arithmeticCalculator.calculate(a, b, OperationType.fromSymbol(s));
                     System.out.println("결과 : " + calced);
+                    System.out.println("결과 리스트 : " + arithmeticCalculator.getResults());
+                    System.out.print("임계값을 입력해주세요 : ");
+                    double threshold = scanner.nextDouble();
+                    scanner.nextLine();
+                    System.out.print("임계값보다 큰 결과 : ");
+                    arithmeticCalculator.getResultsGreaterThan(threshold).forEach(result -> {
+                        System.out.print(result + " ");
+                    });
                     System.out.print("종료하시려면 exit, 아니면 아무 키나 입력해주세요 : ");
                 } while (!scanner.nextLine().equals("exit"));
             } catch (RuntimeException e) {
