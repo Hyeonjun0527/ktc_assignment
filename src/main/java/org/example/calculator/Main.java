58ea08d6837f0cd9469eee77610d7bb3d981cbe6
HyeonJun <osumainaddict527@gmail.com>
2025-05-04 23:49:15 +0900

    수정

--- src/main/java/org/example/calculator/Main.java
+++ src/main/java/org/example/calculator/Main.java
@@ -15,7 +15,6 @@ public class Main {
                     double b = scanner.nextDouble();
                     System.out.print("사칙연산 기호를 입력하세요: ");
                     char s = scanner.next().charAt(0);
-                    scanner.nextLine();
                     //처음에 nextLine으로 했었음. nextInt()는 개행문자를 그대로 둬서 nextLine()을 호출하면
                     //내용을 없는 빈 줄을 얻게 되어. StringIndexOfBoundsException이 발생했음.
                     //공백, 개행을 건너뛰고 다음 토큰을 읽기 때문에 next로 입력을 받을 수 있었다.
@@ -25,6 +24,8 @@ public class Main {
                     첫 번째 숫자를 입력하세요:
                     가 뜨는 것이다. 그 이유는 개행문자가 그대로 남아. nextLine()이 버퍼에 있었던 개행문자를 읽었다.
                     그리고 즉시 if문이 false가 되어 다시 시작되었다. 즉 무한 반복하는 프로그램을 만든것.
+                    다시 말해 종료하시려면 exit,아니면 아무키나 입력하세요. 에서 자동으로 계속 enter가 입력되어버린것이다.
+                    35번째 줄에서 nextLine()을 호출하지 않았을 때 그런 에러가 났었다.
                      */
                     double calced = arithmeticCalculator.calculate(a, b, OperationType.fromSymbol(s));
                     System.out.println("결과 : " + calced);
@@ -36,7 +37,7 @@ public class Main {
                     arithmeticCalculator.getResultsGreaterThan(threshold).forEach(result -> {
                         System.out.print(result + " ");
                     });
-                    System.out.print("종료하시려면 exit, 아니면 아무 키나 입력해주세요 : ");
+                    System.out.print("\n종료하시려면 exit, 아니면 아무 키나 입력해주세요 : ");
                 } while (!scanner.nextLine().equals("exit"));
             } catch (RuntimeException e) {
                 throw new RuntimeException(e);
